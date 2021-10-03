5348f6b5853c0e4867448d7a3cb3fe2a3c0bccc7
terryinfcco <terryinclmich@hotmail.com>
2021-10-03 09:26:59 -0600

    First Program - Create DB

---
+++ connect.py
@@ -0,0 +1,8 @@
+import sqlite3
+
+# Connect to database
+# if you want to just have a db in memory that disappears after the program ends
+# conn = sqlite3.connect(':memory:')
+# if customer.db exists this will just connect to it rather than recreate it.
+conn = sqlite3.connect('customer.db')
+
